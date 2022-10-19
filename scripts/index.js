319f0af9ef26277c8d277c7f5eb260387716a15c
Victor Alexander <victoryalexander@outlook.com>
2022-10-19 09:32:58 -0700

    Added index.js

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    {
+      name: 'Yosemite Valley',
+      link: '../images/yosemite-valley.jpg'
+    }
+    {
+      name: 'Lake Louise',
+      link: '../images/lake-louise.jpg'
+    }
+    {
+      name: 'Bald Mountains',
+      link: '../images/bald-mountains.jpg'
+    }
+    {
+      name: 'Latemar',
+      link: '../images/latemar.jpg'
+    }
+    {
+      name: 'Vanoise National Park',
+      link: '../images/vanoise-national-park.jpg'
+    }
+    {
+      name: 'Lago di Braies',
+      link: '../images/lago-di-braies'
+    }
+];
