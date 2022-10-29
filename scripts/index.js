efcd984e009fd0ea75c93d2151014ea53acff73f
Victor Alexander <victoryalexander@outlook.com>
2022-10-28 17:05:11 -0700

    Removed hard code cards from html and replaced with javascript array

--- scripts/index.js
+++ scripts/index.js
@@ -25,9 +25,28 @@ const initialCards = [
     }
 ];
 
-let editButton = document.querySelector(".profile__info-button");
-let profileEdit = document.querySelector(".modal");
-let closeButton = document.querySelector(".modal-button");
+const editButton = document.querySelector(".profile__info-button");
+const profileEdit = document.querySelector(".modal");
+const closeButton = document.querySelector(".modal-button");
+const editProfile = document.querySelector(".modal__container");
+const profileName = document.querySelector(".profile__info-name");
+const profileDescription = document.querySelector(".profile__info-description");
+const profileNameInput = document.querySelector(".modal__container-name");
+const profileDescriptionInput = document.querySelector(".modal__container-description");
+const cardList = document.querySelector(".cards__list");
+const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
+
+
+initialCards.forEach(function (data) {
+    const cardElement = cardTemplate.cloneNode(true);
+    const imageElement = cardElement.querySelector(".card__image");
+    const cardTitle = cardElement.querySelector(".card__tab-title");
+    imageElement.src = data.link;
+    imageElement.alt = data.name;
+    cardTitle.textContent = data.name;
+    cardList.appendChild(cardElement);
+});
+
 
 function openModal() {
     profileEdit.classList.add("modal_opened");
@@ -38,4 +57,23 @@ function closeModal() {
 }
 
 editButton.addEventListener("click", openModal);
+
+editButton.addEventListener("click", () => {
+    profileNameInput.value = profileName.textContent;
+    profileDescriptionInput.value = profileDescription.textContent;
+
+    openModal();
+})
+
 closeButton.addEventListener("click", closeModal);
+
+editProfile.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const nameValue = event.target.name.value;
+    const descriptionValue = event.target.description.value;
+
+    profileName.textContent = nameValue;
+    profileDescription.textContent = descriptionValue;
+
+    closeModal();
+});
